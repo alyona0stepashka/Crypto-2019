cee829021f3dda784e11fbb6ec5222efe2fdb449
alyona0stepashka <paspaspas01@yandex.ru>
2019-03-09 14:03:48 +0300

    2

---
+++ 02/02/KriptografyLab2/KriptografyLab2/Alphabet.cs
@@ -0,0 +1,96 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KriptografyLab2
+{
+    class Alphabet
+    {
+        int[] colFx;
+        double[] verSim;
+        char[] cyrillic = {'А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч',
+                           'Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я'};
+        char[] latin = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Y', 'Z' };
+        char[] binary = {'0','1'};
+
+        public char[] Cyrillic { get => cyrillic; set => cyrillic = value; }
+        public char[] Latin { get => latin; set => latin = value; }
+        public char[] Binary { get => binary; set => binary = value; }
+
+        public double Entropy(String file,char [] alf)
+        {
+            colFx = new int[alf.Length];
+            for (int i = 0; i < alf.Length;i++)
+            {
+                colFx[i]=file.ToUpper().Where(el => el == alf[i]).Count();
+                if(colFx[i]!=0)
+                Console.WriteLine(colFx[i]+" "+alf[i]);
+            }
+            verSim = new double[alf.Length];
+            for (int i = 0; i < alf.Length; i++)
+            {
+                if (colFx[i] != 0)
+                {
+                    verSim[i] = ((double)colFx[i] / file.Length);
+                    Console.WriteLine(verSim[i]+" "+alf[i]);
+                }
+            }
+            double res=0;
+            for(int i=0;i<alf.Length;i++)
+            {
+                if(verSim[i]!=0)
+                res += verSim[i] * Math.Log(verSim[i],2);
+            }
+            return (-res);
+        }
+        public double kolInf(double ResEntr,int kolSimv)
+        {
+            return ResEntr * (double)kolSimv;
+        }
+        public int [] GetASCII(String str)
+        {
+            int[] codASCII=new int [str.Length];
+            for(int i=0;i<str.Length;i++)
+            {
+                codASCII[i] = ((int)str[i]);
+            }
+            return codASCII;
+        }
+        public string getBytes(int[] cod)
+        {
+            String strB = "";
+            for(int i=0;i<cod.Length;i++)
+            {
+                strB += Convert.ToString(cod[i], 2)+" ";
+            }
+            return strB;
+        }
+        public double EfectEntropy(double verError)
+        {
+            return 1 - (-verError * Math.Log(verError, 2) - (1 - verError) * Math.Log((1 - verError), 2));
+        }
+        public void BuildExel(char[] alf)
+        {
+            using (ExcelPackage excel = new ExcelPackage())
+            {
+                excel.Workbook.Worksheets.Add("Worksheet1");
+                var headerRow = new List<object[]>();
+                headerRow.Add(new object[] { "NAME", "Value" });
+                for (int i=0;i<colFx.Length;i++)
+                {
+                    if(colFx[i]!=0)
+                    headerRow.Add(new object[] { $"{alf[i]}", colFx[i] });
+                }
+                string headerRange = "A1:" + Char.ConvertFromUtf32(headerRow[0].Length + 64) + "1";
+                var worksheet = excel.Workbook.Worksheets["Worksheet1"];
+                worksheet.Cells[headerRange].LoadFromArrays(headerRow);
+                FileInfo excelFile = new FileInfo(@"D:\colSimv.xlsx");
+                excel.SaveAs(excelFile);
+            }
+        }
+    }
+}
---
+++ 02/02/KriptografyLab2/KriptografyLab2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KriptografyLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String text="";
+            Alphabet alphabet = new Alphabet();
+            using (StreamReader sr = new StreamReader("D:/alf.txt"))
+            {
+                text=(sr.ReadToEnd());
+                text = text.Replace(" ","");
+                Console.WriteLine(text);
+            }
+            String text2 = "";
+            Alphabet alphabet2 = new Alphabet();
+            using (StreamReader sr = new StreamReader("D:/alf2.txt"))
+            {
+                text2 = (sr.ReadToEnd());
+                text2 = text.Replace(" ", "");
+                Console.WriteLine(text2);
+            }
+            Console.WriteLine("Entropy: " + alphabet.Entropy(text, alphabet.Cyrillic));
+            Console.WriteLine("Entropy: " + alphabet.Entropy(text, alphabet.Latin));
+            alphabet.BuildExel(alphabet.Latin);
+            Console.WriteLine("Entropy: " + alphabet.Entropy(text, alphabet.Binary));
+             
+            Console.WriteLine("Infor: " + alphabet.kolInf(alphabet2.Entropy(text2, alphabet2.Latin), text2.Length));
+
+            using (StreamWriter sw = new StreamWriter("D:/byt.txt", false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(alphabet.getBytes(alphabet.GetASCII("pakholkoalyonastepanovna")));
+            }
+
+            using (StreamReader sr = new StreamReader("D:/byt.txt"))
+            {
+                text = (sr.ReadToEnd());
+                text = text.Replace(" ", "");
+            }
+
+            Console.WriteLine("Infor: " + alphabet.kolInf(alphabet.Entropy(text, alphabet.Binary), text.Length));
+            Console.WriteLine("Infor: ERROR 0.1 "+alphabet.EfectEntropy(0.1));
+            Console.WriteLine("Infor: ERROR 0.5 " + alphabet.EfectEntropy(0.5));
+            Console.WriteLine("Infor: ERROR 1 " + alphabet.EfectEntropy(1));
+            Console.ReadKey();
+        }
+    }
+}
