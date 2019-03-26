6b792d0055829ae179cced1c94b239771d35afdb
alyona0stepashka <paspaspas01@yandex.ru>
2019-03-26 16:19:00 +0300

    5

---
+++ 05/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class Class1
+    {
+        public static int GetNOD(int val1, int val2)
+        {
+            val1 = Math.Abs(val1);
+            val2 = Math.Abs(val2);
+            if (val2 == 0)
+                return val1;
+            else
+                return GetNOD(val2, val1 % val2);
+        }
+
+        public static double BinModPow(double a, double b, double n)
+        {
+            decimal x = (decimal)b;
+            int i = 0;
+            do
+            {
+                if (x % 2 == 1) x--;
+                x = x / 2;
+                i++;
+            }
+            while (x > 0);
+            x = (decimal)b;
+            decimal[] ar = new decimal[i];
+            for (i = 0; i < ar.Length; i++)
+            {
+                ar[i] = x % 2;
+                if (x % 2 == 1) x--;
+                x = x / 2;
+            }
+            x = (decimal)a;
+            for (i = ar.Length - 2; i >= 0; i--)
+            {
+                x = (x * x) % (decimal)n;
+                x = (x * (decimal)Math.Pow(a, (double)ar[i])) % (decimal)n;
+            }
+            return (double)x;
+        }
+    }
+}
---
+++ 05/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------NOD(21,3) = " + Class1.GetNOD(21, 3));
+            Console.WriteLine("-------Reverce by n (a=12,b=8,n=2) = " + Class1.BinModPow(12, 8, 2));
+            Console.ReadKey();
+        }
+    }
+}
